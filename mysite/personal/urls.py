ebdd2dcc00ba7b72c725f495f7c9c985fa7ce9e1
Gaurav <rawat271165@gmail.com>
2017-09-28 11:49:46 +0530

    idk

--- mysite/personal/urls.py
+++ mysite/personal/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import url,include
 from . import views
 urlpatterns = [
     url(r'^$', views.index,name='index'),
+    url(r'^help$', views.help,name='help'),
 
 ]
--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personal/header.html')
+
+def help(request):
+    return render(request, 'personal/help.html',{'content':['Gaurav','Rawat','New','site']})
